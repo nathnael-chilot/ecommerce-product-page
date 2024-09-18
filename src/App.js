687df3871920836e595146b0c1a41755743d565c
nathnael-chilot <chilotnathnael@gmail.com>
2024-09-18 15:33:42 +0300

    create a react app with tailwind css

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <p className="text-2xl font-bold">Hello, Frontend Mentor!!!</p>
+    </div>
+  );
+}
+
+export default App;
